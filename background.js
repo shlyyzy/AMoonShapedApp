108b8c7e0578c52c5df0c57b05b8ef41148fb4cb
shlyyzy <shirleyy0103@gmail.com>
2019-08-23 00:03:23 -0700

    added background.js and content.js files, jquery, etc

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {    // add an action here});
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
