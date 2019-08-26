208e1d5e12b37a4a4e7db35d72f54992a155c6e3
shlyyzy <shirleyy0103@gmail.com>
2019-08-26 12:06:13 -0700

    deleted alert; only notification now

--- background.js
+++ background.js
@@ -1,24 +1,4 @@
 'use strict';
-var dbName = 'sleepTimeInfo';
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
-
-// displaying notification
-function showNotification(){
-  var opt = {
-        type: 'basic',
-        iconUrl: 'moon.png',
-        title: 'Go to sleep!',
-        message: "Sleep is important! Go to sleep now.",
-  };
-  chrome.notifications.create('reminder', opt);
-}
 
 // show notification once alarm goes off
 chrome.alarms.onAlarm.addListener(function(alarm){
@@ -26,8 +6,15 @@ chrome.alarms.onAlarm.addListener(function(alarm){
         type: 'basic',
         iconUrl: chrome.extension.getURL('/moon.png'),
         title: 'Go to sleep!',
-        message: "Sleep is important! Go to sleep now.",
+        message: "Sleep is important! Go to sleep now."
   };
   chrome.notifications.create('reminder', opt);
-  alert("go to sleep!!");
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
 });
